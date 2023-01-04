153916d3f2554385dbca4db55f5a3b0c0ddf041f
João Frade <joao.guedes2@fatec.sp.gov.br>
2023-01-04 03:31:04 -0300

    refatorando

--- src/acoes.js
+++ src/acoes.js
@@ -1,4 +1,4 @@
-fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR3?range=1d&interval=1d&fundamental=false`).then(response => {
+fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR3%2CVIVT3%2CGGBR3%2CUNIP3%2CWEGE3%2CVIVA3%2CITSA3?fundamental=false`).then(response => {
     return response.json()
 }).then( data => {
 
@@ -61,7 +61,67 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     let SAPOpen = document.getElementById('SAPOpen')
     let SAPImg = document.getElementById('SAPImg')
     let SAPPercent = document.getElementById('SAPPercent')
-    
+
+    //vivo
+    let VIVName = document.getElementById('VIVName')
+    let VIVSymbol = document.getElementById('VIVSymbol')
+    let VIVPrice = document.getElementById('VIVPrice')
+    let VIVMax = document.getElementById('VIVMax')
+    let VIVMin = document.getElementById('VIVMin')
+    let VIVOpen = document.getElementById('VIVOpen')
+    let VIVImg = document.getElementById('VIVImg')
+    let VIVPercent = document.getElementById('VIVPercent')
+
+    //gerdau
+    let GERName = document.getElementById('GERName')
+    let GERSymbol = document.getElementById('GERSymbol')
+    let GeRPrice = document.getElementById('GERPrice')
+    let GERMax = document.getElementById('GERMax')
+    let GERMin = document.getElementById('GERMin')
+    let GEROpen = document.getElementById('GEROpen')
+    let GERImg = document.getElementById('GERImg')
+    let GERPercent = document.getElementById('GERPercent')
+
+    //unipar
+    let UNIName = document.getElementById('UNIName')
+    let UNISymbol = document.getElementById('UNISymbol')
+    let UNIPrice = document.getElementById('UNIPrice')
+    let UNIMax = document.getElementById('UNIMax')
+    let UNIMin = document.getElementById('UNIMin')
+    let UNIOpen = document.getElementById('UNIOpen')
+    let UNIImg = document.getElementById('UNIImg')
+    let UNIPercent = document.getElementById('UNIPercent')
+
+    //weg
+    let WEGName = document.getElementById('WEGName')
+    let WEGSymbol = document.getElementById('WEGSymbol')
+    let WEGPrice = document.getElementById('WEGPrice')
+    let WEGMax = document.getElementById('WEGMax')
+    let WEGMin = document.getElementById('WEGMin')
+    let WEGOpen = document.getElementById('WEGOpen')
+    let WEGImg = document.getElementById('WEGImg')
+    let WEGPercent = document.getElementById('WEGPercent')
+
+    //vivara
+    let VARName = document.getElementById('VARName')
+    let VARSymbol = document.getElementById('VARSymbol')
+    let VARPrice = document.getElementById('VARPrice')
+    let VARMax = document.getElementById('VARMax')
+    let VARMin = document.getElementById('VARMin')
+    let VAROpen = document.getElementById('VAROpen')
+    let VARImg = document.getElementById('VARImg')
+    let VARPercent = document.getElementById('VARPercent')
+
+    //itausa
+    let ITAName = document.getElementById('ITAName')
+    let ITASymbol = document.getElementById('ITASymbol')
+    let ITAPrice = document.getElementById('ITAPrice')
+    let ITAMax = document.getElementById('ITAMax')
+    let ITAMin = document.getElementById('ITAMin')
+    let ITAOpen = document.getElementById('ITAOpen')
+    let ITAImg = document.getElementById('ITAImg')
+    let ITAPercent = document.getElementById('ITAPercent')
+
     //-----------------------------------------------------------
 
     //banco do brasil
@@ -124,6 +184,66 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     SAPPercent.innerHTML = data.results[5].regularMarketChangePercent.toFixed(2) + `%`
     SAPImg.src = data.results[5].logourl
 
+    //vivo
+    VIVName.innerHTML = data.results[6].longName     
+    VIVSymbol.innerHTML = data.results[6].symbol
+    VIVPrice.innerHTML += data.results[6].regularMarketPrice + ` <span> ${data.results[0].currency}</span>`
+   // VIVMax.innerHTML += data.results[6].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
+   // VIVMin.innerHTML += data.results[6].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
+   // VIVOpen.innerHTML += data.results[6].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
+    VIVPercent.innerHTML = data.results[6].regularMarketChangePercent.toFixed(2) + `%`
+    VIVImg.src = data.results[6].logourl
+
+    //gerdau
+    GERName.innerHTML = data.results[7].longName     
+    GERSymbol.innerHTML = data.results[7].symbol
+    GERPrice.innerHTML += data.results[7].regularMarketPrice + ` <span> ${data.results[0].currency}</span>`
+   // GERMax.innerHTML += data.results[7].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
+   // GERMin.innerHTML += data.results[7].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
+   // GEROpen.innerHTML += data.results[7].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
+    GERPercent.innerHTML = data.results[7].regularMarketChangePercent.toFixed(2) + `%`
+    GERImg.src = data.results[7].logourl
+
+    //unipar
+    UNIName.innerHTML = data.results[8].longName     
+    UNISymbol.innerHTML = data.results[8].symbol
+    UNIPrice.innerHTML += data.results[8].regularMarketPrice + ` <span> ${data.results[0].currency}</span>`
+   // UNIMax.innerHTML += data.results[8].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
+   // UNIMin.innerHTML += data.results[8].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
+   // UNIOpen.innerHTML += data.results[8].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
+    UNIPercent.innerHTML = data.results[8].regularMarketChangePercent.toFixed(2) + `%`
+    UNIImg.src = data.results[8].logourl
+
+    //weg
+    WEGName.innerHTML = data.results[9].longName     
+    WEGSymbol.innerHTML = data.results[9].symbol
+    WEGPrice.innerHTML += data.results[9].regularMarketPrice + ` <span> ${data.results[0].currency}</span>`
+   // WEGMax.innerHTML += data.results[9].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
+   // WEGMin.innerHTML += data.results[9].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
+   // WEGOpen.innerHTML += data.results[9].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
+    WEGPercent.innerHTML = data.results[9].regularMarketChangePercent.toFixed(2) + `%`
+    WEGImg.src = data.results[9].logourl
+
+    //vivara
+    VARName.innerHTML = data.results[10].longName     
+    VARSymbol.innerHTML = data.results[10].symbol
+    VARPrice.innerHTML += data.results[10].regularMarketPrice + ` <span> ${data.results[0].currency}</span>`
+   // VARMax.innerHTML += data.results[10].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
+   // VARMin.innerHTML += data.results[10].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
+   // VAROpen.innerHTML += data.results[10].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
+    VARPercent.innerHTML = data.results[10].regularMarketChangePercent.toFixed(2) + `%`
+    VARImg.src = data.results[10].logourl
+
+    //itausa
+    ITAName.innerHTML = data.results[11].longName     
+    ITASymbol.innerHTML = data.results[11].symbol
+    ITAPrice.innerHTML += data.results[11].regularMarketPrice + ` <span> ${data.results[0].currency}</span>`
+   // ITAMax.innerHTML += data.results[11].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
+   // ITAMin.innerHTML += data.results[11].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
+   // ITAOpen.innerHTML += data.results[11].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
+    ITAPercent.innerHTML = data.results[11].regularMarketChangePercent.toFixed(2) + `%`
+    ITAImg.src = data.results[11].logourl
+
 
 // function - green positve - red negative
     function percentColor() {
@@ -137,6 +257,12 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
         const percent3 = data.results[3].regularMarketChangePercent
         const percent4 = data.results[4].regularMarketChangePercent
         const percent5 = data.results[5].regularMarketChangePercent
+        const percent6 = data.results[6].regularMarketChangePercent
+        const percent7 = data.results[7].regularMarketChangePercent
+        const percent8 = data.results[8].regularMarketChangePercent
+        const percent9 = data.results[9].regularMarketChangePercent
+        const percent10 = data.results[10].regularMarketChangePercent
+        const percent11 = data.results[11].regularMarketChangePercent
 
         if (percent0 < 0) BBPercent.style.color =  red;
         else BBPercent.style.color = green;
@@ -155,6 +281,25 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
 
         if (percent5 < 0) SAPPercent.style.color = red;
         else SAPPercent.style.color = green;
+
+        if (percent6 < 0) VIVPercent.style.color = red;
+        else VIVPercent.style.color = green;
+
+        if (percent7 < 0) GERPercent.style.color = red;
+        else GERPercent.style.color = green;
+
+        if (percent8 < 0) UNIPercent.style.color = red;
+        else UNIPercent.style.color = green;
+
+        if (percent9 < 0) WEGPercent.style.color = red;
+        else WEGPercent.style.color = green;
+
+        if (percent10 < 0) VARPercent.style.color = red;
+        else VARPercent.style.color = green;
+
+        if (percent11 < 0) ITAPercent.style.color = red;
+        else ITAPercent.style.color = green;
+
     }
     percentColor()
 })
