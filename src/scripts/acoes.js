627090a262461523aa26202332aa67aef8eb5ae3
João Frade <joao.guedes2@fatec.sp.gov.br>
2023-01-27 18:55:44 -0300

    att

--- src/scripts/acoes.js
+++ src/scripts/acoes.js
@@ -6,9 +6,9 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     let BBName = document.getElementById('BBName')
     let BBSymbol = document.getElementById('BBSymbol')
     let BBPrice = document.getElementById('BBPrice')
-    let BBMax = document.getElementById('BBMax')
-    let BBMin = document.getElementById('BBMin')
-    let BBOpen = document.getElementById('BBOpen')
+  //  let BBMax = document.getElementById('BBMax')
+  //  let BBMin = document.getElementById('BBMin')
+  // let BBOpen = document.getElementById('BBOpen')
     let BBImg = document.getElementById('BBImg')
     let BBPercent = document.getElementById('BBPercent')
 
@@ -26,9 +26,9 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     let TAEName = document.getElementById('TAEName')
     let TAESymbol = document.getElementById('TAESymbol')
     let TAEPrice = document.getElementById('TAEPrice')
-    let TAEMax = document.getElementById('TAEMax')
-    let TAEMin = document.getElementById('TAEMin')
-    let TAEOpen = document.getElementById('TAEOpen')
+   // let TAEMax = document.getElementById('TAEMax')
+   // let TAEMin = document.getElementById('TAEMin')
+   // let TAEOpen = document.getElementById('TAEOpen')
     let TAEImg = document.getElementById('TAEImg')
     let TAEPercent = document.getElementById('TAEPercent')
 
@@ -46,9 +46,9 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     let VALName = document.getElementById('VALName')
     let VALSymbol = document.getElementById('VALSymbol')
     let VALPrice = document.getElementById('VALPrice')
-    let VALMax = document.getElementById('VALMax')
-    let VALMin = document.getElementById('VALMin')
-    let VALOpen = document.getElementById('VALOpen')
+   // let VALMax = document.getElementById('VALMax')
+  //  let VALMin = document.getElementById('VALMin')
+   // let VALOpen = document.getElementById('VALOpen')
     let VALImg = document.getElementById('VALImg')
     let VALPercent = document.getElementById('VALPercent')
 
@@ -66,18 +66,18 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     let VIVName = document.getElementById('VIVName')
     let VIVSymbol = document.getElementById('VIVSymbol')
     let VIVPrice = document.getElementById('VIVPrice')
-    let VIVMax = document.getElementById('VIVMax')
-    let VIVMin = document.getElementById('VIVMin')
-    let VIVOpen = document.getElementById('VIVOpen')
+    //let VIVMax = document.getElementById('VIVMax')
+   // let VIVMin = document.getElementById('VIVMin')
+    //let VIVOpen = document.getElementById('VIVOpen')
     let VIVImg = document.getElementById('VIVImg')
     let VIVPercent = document.getElementById('VIVPercent')
 
     //gerdau
     let GERName = document.getElementById('GERName')
     let GERSymbol = document.getElementById('GERSymbol')
-    let GeRPrice = document.getElementById('GERPrice')
-    let GERMax = document.getElementById('GERMax')
-    let GERMin = document.getElementById('GERMin')
+   // let GeRPrice = document.getElementById('GERPrice')
+  //  let GERMax = document.getElementById('GERMax')
+   // let GERMin = document.getElementById('GERMin')
     let GEROpen = document.getElementById('GEROpen')
     let GERImg = document.getElementById('GERImg')
     let GERPercent = document.getElementById('GERPercent')
@@ -86,9 +86,9 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     let UNIName = document.getElementById('UNIName')
     let UNISymbol = document.getElementById('UNISymbol')
     let UNIPrice = document.getElementById('UNIPrice')
-    let UNIMax = document.getElementById('UNIMax')
-    let UNIMin = document.getElementById('UNIMin')
-    let UNIOpen = document.getElementById('UNIOpen')
+   // let UNIMax = document.getElementById('UNIMax')
+   // let UNIMin = document.getElementById('UNIMin')
+   // let UNIOpen = document.getElementById('UNIOpen')
     let UNIImg = document.getElementById('UNIImg')
     let UNIPercent = document.getElementById('UNIPercent')
 
@@ -96,9 +96,9 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     let WEGName = document.getElementById('WEGName')
     let WEGSymbol = document.getElementById('WEGSymbol')
     let WEGPrice = document.getElementById('WEGPrice')
-    let WEGMax = document.getElementById('WEGMax')
-    let WEGMin = document.getElementById('WEGMin')
-    let WEGOpen = document.getElementById('WEGOpen')
+    //let WEGMax = document.getElementById('WEGMax')
+   // let WEGMin = document.getElementById('WEGMin')
+   // let WEGOpen = document.getElementById('WEGOpen')
     let WEGImg = document.getElementById('WEGImg')
     let WEGPercent = document.getElementById('WEGPercent')
 
@@ -106,9 +106,9 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     let VARName = document.getElementById('VARName')
     let VARSymbol = document.getElementById('VARSymbol')
     let VARPrice = document.getElementById('VARPrice')
-    let VARMax = document.getElementById('VARMax')
-    let VARMin = document.getElementById('VARMin')
-    let VAROpen = document.getElementById('VAROpen')
+   // let VARMax = document.getElementById('VARMax')
+   // let VARMin = document.getElementById('VARMin')
+   // let VAROpen = document.getElementById('VAROpen')
     let VARImg = document.getElementById('VARImg')
     let VARPercent = document.getElementById('VARPercent')
 
@@ -116,9 +116,9 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     let ITAName = document.getElementById('ITAName')
     let ITASymbol = document.getElementById('ITASymbol')
     let ITAPrice = document.getElementById('ITAPrice')
-    let ITAMax = document.getElementById('ITAMax')
-    let ITAMin = document.getElementById('ITAMin')
-    let ITAOpen = document.getElementById('ITAOpen')
+   // let ITAMax = document.getElementById('ITAMax')
+    //let ITAMin = document.getElementById('ITAMin')
+    //let ITAOpen = document.getElementById('ITAOpen')
     let ITAImg = document.getElementById('ITAImg')
     let ITAPercent = document.getElementById('ITAPercent')
 
@@ -128,9 +128,9 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     BBName.innerHTML = data.results[0].longName     
     BBSymbol.innerHTML = data.results[0].symbol
     BBPrice.innerHTML += data.results[0].regularMarketPrice + ` <span> ${data.results[0].currency}</span>`
-    BBMax.innerHTML += data.results[0].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
-    BBMin.innerHTML += data.results[0].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
-    BBOpen.innerHTML += data.results[0].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
+    //BBMax.innerHTML += data.results[0].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
+   // BBMin.innerHTML += data.results[0].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
+    //BBOpen.innerHTML += data.results[0].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
     BBPercent.innerHTML = data.results[0].regularMarketChangePercent.toFixed(2) + `%`
     BBImg.src = data.results[0].logourl
 
@@ -138,9 +138,9 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     PTRName.innerHTML = data.results[1].longName     
     PTRSymbol.innerHTML = data.results[1].symbol
     PTRPrice.innerHTML += data.results[1].regularMarketPrice + ` <span> ${data.results[0].currency}</span>`
-    PTRMax.innerHTML += data.results[1].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
-    PTRMin.innerHTML += data.results[1].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
-    PTROpen.innerHTML += data.results[1].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
+   // PTRMax.innerHTML += data.results[1].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
+   // PTRMin.innerHTML += data.results[1].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
+   // PTROpen.innerHTML += data.results[1].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
     PTRPercent.innerHTML = data.results[1].regularMarketChangePercent.toFixed(2) + `%`
     PTRImg.src = data.results[1].logourl
 
@@ -148,9 +148,9 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     TAEName.innerHTML = data.results[2].longName     
     TAESymbol.innerHTML = data.results[2].symbol
     TAEPrice.innerHTML += data.results[2].regularMarketPrice + ` <span> ${data.results[0].currency}</span>`
-    TAEMax.innerHTML += data.results[2].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
-    TAEMin.innerHTML += data.results[2].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
-    TAEOpen.innerHTML += data.results[2].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
+    //TAEMax.innerHTML += data.results[2].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
+    //TAEMin.innerHTML += data.results[2].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
+    //TAEOpen.innerHTML += data.results[2].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
     TAEPercent.innerHTML = data.results[2].regularMarketChangePercent.toFixed(2) + `%`
     TAEImg.src = data.results[2].logourl
 
@@ -158,9 +158,9 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     COPName.innerHTML = data.results[3].longName     
     COPSymbol.innerHTML = data.results[3].symbol
     COPPrice.innerHTML += data.results[3].regularMarketPrice + ` <span> ${data.results[0].currency}</span>`
-    COPMax.innerHTML += data.results[3].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
-    COPMin.innerHTML += data.results[3].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
-    COPOpen.innerHTML += data.results[3].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
+    //COPMax.innerHTML += data.results[3].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
+    //COPMin.innerHTML += data.results[3].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
+    //COPOpen.innerHTML += data.results[3].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
     COPPercent.innerHTML = data.results[3].regularMarketChangePercent.toFixed(2) + `%`
     COPImg.src = data.results[3].logourl
 
@@ -168,9 +168,9 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     VALName.innerHTML = data.results[4].longName     
     VALSymbol.innerHTML = data.results[4].symbol
     VALPrice.innerHTML += data.results[4].regularMarketPrice + ` <span> ${data.results[0].currency}</span>`
-    VALMax.innerHTML += data.results[4].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
-    VALMin.innerHTML += data.results[4].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
-    VALOpen.innerHTML += data.results[4].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
+    //VALMax.innerHTML += data.results[4].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
+    //VALMin.innerHTML += data.results[4].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
+    //VALOpen.innerHTML += data.results[4].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
     VALPercent.innerHTML = data.results[4].regularMarketChangePercent.toFixed(2) + `%`
     VALImg.src = data.results[4].logourl
 
@@ -178,9 +178,9 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
     SAPName.innerHTML = data.results[5].longName     
     SAPSymbol.innerHTML = data.results[5].symbol
     SAPPrice.innerHTML += data.results[5].regularMarketPrice + ` <span> ${data.results[0].currency}</span>`
-    SAPMax.innerHTML += data.results[5].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
-    SAPMin.innerHTML += data.results[5].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
-    SAPOpen.innerHTML += data.results[5].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
+    //SAPMax.innerHTML += data.results[5].regularMarketDayHigh + ` <span> ${data.results[0].currency}</span>`
+    //SAPMin.innerHTML += data.results[5].regularMarketDayLow + ` <span> ${data.results[0].currency}</span>`
+    //SAPOpen.innerHTML += data.results[5].regularMarketOpen + `<span> ${data.results[0].currency}</span>`
     SAPPercent.innerHTML = data.results[5].regularMarketChangePercent.toFixed(2) + `%`
     SAPImg.src = data.results[5].logourl
 
@@ -298,68 +298,8 @@ fetch(`https://brapi.dev/api/quote/BBAS3%2CPETR4%2CTAEE11%2CCPLE3%2CVALE3%2CSAPR
         else VARPercent.style.color = green;
 
         if (percent11 < 0) ITAPercent.style.color = red;
-        else ITAPercent.style.color = green;
+        else ITAPercent.style.color = green;//
 
     }
     percentColor()
 })
-
-    let showPopup1 = document.getElementById('showPopup1')
-    showPopup1.addEventListener('click', function() {
-        document.getElementById('popup1').classList.add("active");
-    })
-    
-    let closePopup1 = document.getElementById('closePopup1')
-    closePopup1.addEventListener('click', function() {
-        document.getElementById('popup1').classList.remove("active");
-    })
-
-    let showPopup2 = document.getElementById('showPopup2')
-    showPopup2.addEventListener('click', function() {
-        document.getElementById('popup2').classList.add("active");
-    })
-    
-    let closePopup2 = document.getElementById('closePopup2')
-    closePopup2.addEventListener('click', function() {
-        document.getElementById('popup2').classList.remove("active");
-    })
-
-    let showPopup3 = document.getElementById('showPopup3')
-    showPopup3.addEventListener('click', function() {
-        document.getElementById('popup3').classList.add("active");
-    })
-    
-    let closePopup3 = document.getElementById('closePopup3')
-    closePopup3.addEventListener('click', function() {
-        document.getElementById('popup3').classList.remove("active");
-    })
-    
-    let showPopup4 = document.getElementById('showPopup4')
-    showPopup4.addEventListener('click', function() {
-        document.getElementById('popup4').classList.add("active");
-    })
-    
-    let closePopup4 = document.getElementById('closePopup4')
-    closePopup4.addEventListener('click', function() {
-        document.getElementById('popup4').classList.remove("active");
-    })
-
-    let showPopup5 = document.getElementById('showPopup5')
-    showPopup5.addEventListener('click', function() {
-        document.getElementById('popup5').classList.add("active");
-    })
-    
-    let closePopup5 = document.getElementById('closePopup5')
-    closePopup5.addEventListener('click', function() {
-        document.getElementById('popup5').classList.remove("active");
-    })
-
-    let showPopup6 = document.getElementById('showPopup6')
-    showPopup6.addEventListener('click', function() {
-        document.getElementById('popup6').classList.add("active");
-    })
-    
-    let closePopup6 = document.getElementById('closePopup6')
-    closePopup6.addEventListener('click', function() {
-        document.getElementById('popup6').classList.remove("active");
-    })
