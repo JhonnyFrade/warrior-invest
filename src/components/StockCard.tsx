3892b39e97818abd233c9242b3c4ce2f6456d677
João Frade <joao.guedes2@fatec.sp.gov.br>
2023-02-06 16:08:18 -0300

    odeio array

--- src/components/StockCard.tsx
+++ src/components/StockCard.tsx
@@ -1,55 +1,34 @@
-import { useEffect, useState } from 'react'
+import { useApi } from '../hooks/useApi'
 import '../styles/StockCard.css'
 
-type stock = {
-    
-    results: [
-        {
-            symbol: string
-        }
-    ] 
-    
-    
+type Stock = {
+    results: [{
+        symbol: string
+    }]
 }
 
 export function StockCard() {
 
-    const [stocks, setStocks] = useState<stock[]>([])
-    
-    useEffect(() => {
-        fetch('https://brapi.dev/api/quote/BBAS3%2CITUB4%2CITSA4%2CBBSE3%2CPETR4%2CVALE3%2CSUZB3%2CVIVT3%2CTAEE4%2CCPLE3%2CSAPR4%2CSBSP3%2CWEGE3%2CGOAU4%2CGGBR4%2CKLBN11%2CFLRY3%2COIBR3%2CMGLU3%2CVIVA3?range=1d&interval=1d&fundamental=true')
-        .then(response => response.json())
-        .then(data => {
-            setStocks(data)
-        })
-    }, [])
+    const { data: dataStocks } = useApi<Stock[]>('https://brapi.dev/api/quote/BBAS3%2CITUB4%2CITSA4%2CBBSE3%2CPETR4%2CVALE3%2CSUZB3%2CVIVT3%2CTAEE4%2CCPLE3%2CSAPR4%2CSBSP3%2CWEGE3%2CGOAU4%2CGGBR4%2CKLBN11%2CFLRY3%2COIBR3%2CMGLU3%2CVIVA3?range=1d&interval=1d&fundamental=true')
 
-    
+  console.log(dataStocks)
+
+
 
     return (
         <div className="card">
-            {stocks.map(repo => {
+            {dataStocks?.map(repo => {
                 return (
+                    <>
                      <div className="main-content" key={repo.results[0].symbol}>
                         <h2>Nome</h2>
                         <h3>{repo.results[0].symbol}</h3>
                         <p>A partir de R$ <span></span> </p>
                      </div>
+                    <div className="percent"><p>1%</p></div>
+                    </>
                 )
             })}
-
-           {
-               /*            
-            <div className="img"></div>
-           <div className="main-content">
-                <h2>Nome da ação</h2>
-                <h3>codigo da ação</h3>
-                <p>A partir de R$ <span>30,00</span> </p>
-            </div>
-            */
-           } 
-
-            <div className="percent"><p>1%</p></div>
         </div>
     )
 }
